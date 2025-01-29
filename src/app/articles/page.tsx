a31c4c661a2e89cb14df99e2044120ced1591a14
CaioHVectorA <caihebatista@gmail.com>
2025-01-29 13:02:11 -0300

    fix: sort articles by date and update date format to Portuguese locale

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,15 +1,18 @@
 import { Article, getArticles } from "@/src/lib/articles";
 import moment from "moment";
 import Link from "next/link";
-
+import 'moment/locale/pt-br'
 export default async function ArticlesPage() {
   const articles = getArticles();
+  const sorted = articles.sort((a, b) => {
+    return moment(a.date, "DD-MM-YYYY").isBefore(moment(b.date, "DD-MM-YYYY")) ? 1 : -1;
+  });
   return (
     <div className="w-full">
-      <h1 className="my-12 font-bold max-sm:text-center">Articles</h1>
+      <h1 className="my-12 font-bold max-sm:text-center">Artigos {process.env.NODE_ENV}</h1>
 
       <section className="flex flex-col divide-y divide-zinc-400 dark:divide-zinc-700 w-full">
-        {articles.map((article: Article) => (
+        {sorted.map((article: Article) => (
           <Link
             className="flex sm:items-center flex-col sm:flex-row justify-between w-full py-4 px-2 hover:bg-zinc-200 dark:hover:bg-zinc-800"
             key={article.id}
@@ -17,7 +20,7 @@ export default async function ArticlesPage() {
           >
             {article.title}{" "}
             <span className="text-zinc-500 dark:text-zinc-400">
-              {moment(article.date, "YYYY-MM-DD").format("MMMM Do, YYYY")}
+              {moment(article.date, "DD-MM-YYYY").locale('pt-br').format("MMMM D, YYYY")}
             </span>
           </Link>
         ))}
